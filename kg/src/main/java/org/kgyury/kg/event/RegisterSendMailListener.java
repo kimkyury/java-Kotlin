d95f7d7ceff8973d05d87ad5fe609683e7840d54
KGYURY <mandarining0918@gmail.com>
2024-12-10 11:15:10 +0900

    Feat: Apply Async SpringEvent

--- kg/src/main/java/org/kgyury/kg/event/RegisterSendMailListener.java
+++ kg/src/main/java/org/kgyury/kg/event/RegisterSendMailListener.java
@@ -3,14 +3,17 @@ package org.kgyury.kg.event;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class RegisterSendMailListener {
 
+    @Async
     @EventListener
     public void handleRegisterEvent(RegisterEvent event){
       log.info("[EVENT LISTENER2] SEND EMAIL: {}", event.getEmail());
+      log.info("[EVENT THREAD] Listener executed on thread: {}" , Thread.currentThread().getName());
     }
 }
