3f7079769cdc6314a5d6957719300acf8c29eff7
unknown <6240029@BNKFGEX.COM>
2025-01-16 17:06:24 +0900

    Feat: Solve Simple Problem about Syntax

--- syntax/ForAndWhile.kt
+++ syntax/ForAndWhile.kt
@@ -34,4 +34,45 @@ fun main (){
         println(i)
     }
 
+
+    /* while */
+    println("-----------------")
+
+    var count = 3
+    while (count > 0){
+        println("$count")
+        count--
+    }
+
+    println("-----------------")
+    do {
+        println("$count")
+    }while (count > 3)
+
+    println("-----------------")
+
+    // 1 부터 100까지 숫자 출력하기
+    for(i in 1..100){
+        print("$i "  )
+    }
+    println("-----------------")
+
+    // 1부터 100사이의 짝수만 출력하기
+    var num = 1
+    while(num < 101){
+        print("$num " )
+        num +=2
+    }
+
+    // when을 통해 숫자를 스트링 치환하기
+    // 사용자의 입력받기, 변환 실패 시에는 Null을 가짐
+    println("-----------------")
+
+    val input = readLine()?.toIntOrNull()
+    val numStr = when {
+        input == 1 -> "하나"
+        input == 2 -> "둘"
+        else -> "기타"
+    }
+    print("$numStr")
 }
