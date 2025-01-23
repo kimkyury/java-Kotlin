ed156c67e9509104f54137d2f66851d53e8993c9
kgyury <mandarining0918@gmail.com>
2025-01-23 16:55:57 +0900

    Feat: Implement Show, Mark Function

--- todoTest/Runner.kt
+++ todoTest/Runner.kt
@@ -20,12 +20,34 @@ class TodoManager{
      fun removeTodo(id: Int){
 
         val removed = todoList.removeIf {it.id == id}
-
         if (removed){
-            println("Deleted TODO: $removed.title")
+            println("Deleted TODO: ${removed.title}")
         }else{
             println("그 ID는 없는 건디")
         }
+    }
+
+    fun showTodos(){
+        if (todoList.isEmpty()){
+            println("비어있습니다.")
+            return
+        }
+
+        todoList.forEach { todo ->
+            println("ID: ${todo.id}, 제목: ${todo.title}, 완료 여부: ${if (todo.isCompleted) "완료" else "미완료"}")
+        }
+    }
+
+    fun markCompleted(id: Int){
+
+        val todo = todolist.find{it.id == id}
+        if ( todo != null){
+            todo.isCompleted = true
+            println("완료: ${todo.id})
+        } else{
+            println("찾을 수 없습니다")
+         }
+
     }
 }
 
@@ -38,6 +60,8 @@ fun main (){
         println("-----START------")
         println("1. ADD TODO")
         println("2. Delete TODO")
+        println("3. Show TODO")
+        println("4. Make Complete TODO")
 
 
         when (readLine()?.toIntOrNull()){
@@ -52,6 +76,17 @@ fun main (){
                 if ( id != null){
                     manager.removeTodo(id) 
                 }
+            }
+            3 -> {
+                manager.showTodos();
+            }
+            4 -> {
+                print("ID: ")
+                val id = readLine()?.toIntOrNull()
+                if ( id != null){
+                    manager.markCompleted(id)
+                }
+
             }
         }
     }   
